2df074b5bb73ca86d397e4790e7c3d69f3168101
Jimena Choque <jimenachoque.bc@gmail.com>
2019-09-12 16:42:34 -0400

    origin

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into inventario(nombre) values ('Kriss');
+insert into inventario(nombre) values ('Juan');
+insert into inventario(nombre) values ('Luis');
+insert into inventario(nombre) values ('Pepe');
+
