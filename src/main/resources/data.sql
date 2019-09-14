81df5b66e8b63ba69dd441581f3be5d4ff295183
Jimena Choque <jimenachoque.bc@gmail.com>
2019-09-14 19:52:15 -0400

    Prueba 2

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into inventario(nombre) values ('Kriss');
-insert into inventario(nombre) values ('Juan');
-insert into inventario(nombre) values ('Luis');
-insert into inventario(nombre) values ('Pepe');
-
